9f2a521c1c33fb3b2c8408cb6f7ca0546fe0a38b
Adegoke Obasa <adegokeobasa@gmail.com>
2018-10-17 06:48:13 +0200

    save deck to file

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 // Create a new type of deck
@@ -31,4 +33,12 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -3,13 +3,5 @@ package main
 func main() {
 	cards := newDeck()
 
-	// Append does not modify a slice, it only creates a new slice
-	// Slices are immutable
-	cards = append(cards, "Six of spades")
-
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-
-	remainingDeck.print()
+	cards.saveToFile("my_cards")
 }
