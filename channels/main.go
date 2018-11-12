321fb114ec3e907db2d8f53948cec4047acd212b
Adegoke Obasa <adegoke.obasa@deliveryhero.com>
2018-11-12 07:13:43 +0100

    http serial status checker

---
+++ channels/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"http://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLinkStatus(link)
+	}
+}
+
+func checkLinkStatus(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Printf("Link %v might be down\n", link)
+	}
+
+	fmt.Printf("%v is up\n", link)
+}
